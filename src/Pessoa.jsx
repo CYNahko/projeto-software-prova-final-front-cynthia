6d5900d766dfb01866bd70ff3d1ede9ba3f850a4
CYNahko <cynthiayasutake@gmail.com>
2025-11-10 09:24:14 -0300

    quase tudo para a prova

--- src/Pessoa.jsx
+++ src/Pessoa.jsx
@@ -4,8 +4,8 @@ import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
 
-const BASE_URL = "http://localhost:8080";
-//const BASE_URL = "http://18.229.161.129:8080";
+//const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://18.229.161.129:8080";
 
 
 export default function PessoasApp() {
