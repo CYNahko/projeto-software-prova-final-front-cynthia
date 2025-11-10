709a4a66bd688864679532d30d9dbe6f9ded55aa
CYNahko <cynthiayasutake@gmail.com>
2025-11-10 11:04:25 -0300

    front na teoria está pronto

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PessoasApp from './Pessoa'
+import ViagensApp from './Viagem'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <PessoasApp></PessoasApp>
+        <ViagensApp></ViagensApp>
       </div>
     </>
   )
